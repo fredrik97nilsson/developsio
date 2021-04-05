7880ffdd72a79419f9cd426e7fc79c346d189836
AntonDahl <svenssondahl@gmail.com>
2021-04-05 17:34:30 +0200

    hej
    hej

--- src/App.js
+++ src/App.js
@@ -1,4 +1,12 @@
+import React, { Component, ReactFragment }  from 'react';
+import {BrowserRouter, Route, NavLink} from 'react-router-dom';
+import './App.module.css';
+import classes from './App.module.css';
+import Button from './components/Button/Button';
+import AdminPage from './components/AdminPage/AdminPage';
+import ApplicantPage from './components/ApplicantPage/ApplicantPage';
 
+<<<<<<< Updated upstream
 function App() {
   return (
     <div className="App">
@@ -6,6 +14,37 @@ function App() {
       <h1>Anton Was here</h1>
     </div>
   );
+=======
+
+class App extends Component {
+  render () {
+
+
+
+    return (
+     
+          <BrowserRouter>
+            <div className= {classes.App}>
+              <h1 className = {classes.Header}>Login Page</h1> 
+
+                <NavLink to='/admin-page'>
+                  <Button buttonName='Admin'> </Button>
+                </NavLink>
+
+                <NavLink to='/applicant-page'>
+                  <Button buttonName='Applicant'></Button>
+                </NavLink>
+            </div>
+
+          <Route path='/admin-page' component={AdminPage}/>
+          <Route path='/applicant-page' component={ApplicantPage}/>
+
+
+          </BrowserRouter>
+      
+    );
+  }
+>>>>>>> Stashed changes
 }
 
 export default App;
