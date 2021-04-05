baab5a0281f3898f484c087f6893b1bc6c146291
fredrik97nilsson <73641486+fredrik97nilsson@users.noreply.github.com>
2021-04-05 23:24:32 +0200

    En branch där Login kan skrivas

--- src/Components/StartPage/StartPage.js
+++ src/Components/StartPage/StartPage.js
@@ -10,29 +10,7 @@ function StartPage() {
         
 
         <div className={classes.StartPageHeader}>
-
-        <h1>DEVELOPSIO</h1>
-
-        <h4>If you're not among the top 1%, we won't even bother looking at your resumé :)</h4>
-        
-        <h2>Already have an account?</h2>
-        
-        <div className={classes.StartPage}>
-            
-               <NavLink to='/create-account'> <Button 
-                    btnType='Danger'
-                    >No</Button>
-                </NavLink>
-
-                <NavLink to='login-page'>
-                <Button 
-                    btnType='Success'
-                    >Yes</Button>
-                </NavLink>
-        </div>
-        
-        <Route path='/login-page' component={LoginPage} />
-        
+            <LoginPage/>        
         </div>
 
         
