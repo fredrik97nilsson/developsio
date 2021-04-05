935a084a3ba81d8f66dc8db8e06e99ccda39502d
fredrik97nilsson <73641486+fredrik97nilsson@users.noreply.github.com>
2021-04-05 15:03:37 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ function App() {
     <div className="App">
       <h1>Hello World</h1>
       <h1>Fanny och Leon was here</h1>
+      <h2>
+        Och oj vad kul de hade!?!?!?!?
+      </h2>
     </div>
   );
 }
