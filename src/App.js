6d8c8ae2db96d1eaf07260fd9c24236341ac4445
fredrik97nilsson <73641486+fredrik97nilsson@users.noreply.github.com>
2021-04-05 17:35:41 +0200

    Rensar upp

--- src/App.js
+++ src/App.js
@@ -2,11 +2,6 @@
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
-      <h1>Fanny och Leon was here</h1>
-      <h2>
-        Och oj vad kul de hade!?!?!?!?
-      </h2>
     </div>
   );
 }
