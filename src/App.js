18f114e2cf9e0a37fa00281abd3b496a8b13d2ec
adambergman123 <73640809+adambergman123@users.noreply.github.com>
2021-04-05 15:09:31 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,6 +10,8 @@ function App() {
       <h1>Adam was here</h1>
       <h2>Det är kul att klättra i träd</h2>
       <h1>Fanny och Leon was here!!!</h1>
+      <h1>Varför gick bajskorven till polisen?</h1>
+      <p>Den kände sig utpressad</p>
     </div>
   );
 }
