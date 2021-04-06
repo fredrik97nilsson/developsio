2d6068116057af773b7360dd9ad9c476228c0629
fredrik97nilsson <73641486+fredrik97nilsson@users.noreply.github.com>
2021-04-06 12:33:26 +0200

    InputFields

--- src/Components/UI/InputField/InputField.js
+++ src/Components/UI/InputField/InputField.js
@@ -2,8 +2,20 @@ import React from 'react';
 
 import classes from './InputField.module.css';
 
-const inputField = (props) => (
+/*const inputField = (props) => (
     <input className={classes.InputField} placeholder={props.placeholder}>{props.children}</input>
-);
+);*/
 
-export default inputField;
+function InputField(props) {
+    const handleChange = (event) => {
+        props.changed(event.target.value);
+    }
+
+    return(
+        <div>
+            <input value={props.value} onChange={handleChange} />
+        </div>
+    );
+}
+
+export default InputField;
